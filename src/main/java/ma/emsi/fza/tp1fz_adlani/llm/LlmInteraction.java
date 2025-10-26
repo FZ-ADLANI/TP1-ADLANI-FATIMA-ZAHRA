edd68d7b427ef0d2eacc1447c343c3c3fee68e0a
FZAdlani <fatimazahra.adlani@emsi-edu.ma>
2025-10-26 09:56:05 +0100

    Correction:Ajout du record dans llminteraction

--- src/main/java/ma/emsi/fza/tp1fz_adlani/llm/LlmInteraction.java
+++ src/main/java/ma/emsi/fza/tp1fz_adlani/llm/LlmInteraction.java
@@ -1,40 +1,8 @@
 package ma.emsi.fza.tp1fz_adlani.llm;
 
-import java.io.Serializable;
-
-public class LlmInteraction implements Serializable {
-
-    private String questionJson;
-    private String reponseJson;
-    private String reponseExtraite;
-
-    public LlmInteraction(String questionJson, String reponseJson, String reponseExtraite) {
-        this.questionJson = questionJson;
-        this.reponseJson = reponseJson;
-        this.reponseExtraite = reponseExtraite;
-    }
-
-    public String questionJson() {
-        return questionJson;
-    }
-
-    public String reponseJson() {
-        return reponseJson;
-    }
-
-    public String reponseExtraite() {
-        return reponseExtraite;
-    }
-
-    public void setQuestionJson(String questionJson) {
-        this.questionJson = questionJson;
-    }
-
-    public void setReponseJson(String reponseJson) {
-        this.reponseJson = reponseJson;
-    }
-
-    public void setReponseExtraite(String reponseExtraite) {
-        this.reponseExtraite = reponseExtraite;
-    }
+public record LlmInteraction(
+        String questionJson,
+        String reponseJson,
+        String reponseExtraite
+) {
 }
